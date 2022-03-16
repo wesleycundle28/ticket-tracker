5c1f6f666ea76eb329e91f13a5af1fd83a331031
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-15 20:32:36 -0400

    update

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,23 +5,7 @@ import { ToastContainer } from "react-toastify";
 export const Layout = () => {
   return (
     <main className="main-container">
-      <Navbar>
-        <NavItem href="/">Home</NavItem>
-        <NavItem href="/admin/programs">Programs</NavItem>
-        <NavItem href="/admin/employees">Employees</NavItem>
-        <NavItem href="/admin/addprogram">Add Program</NavItem>
-        <NavItem href="/admin/addticket">Add Ticket</NavItem>
-        <NavItem href="/myprograms">My Programs</NavItem>
-        <NavItem href="/mytickets">My Tickets</NavItem>
-        <NavItem href="/login">Login/Register</NavItem>
-        <NavItem href="/logout">Logout</NavItem>
-      </Navbar>
-      <div className="outlet-footer-container">
-        <Outlet />
-        <Footer className="footer-container">
-          <FooterItem>footer</FooterItem>
-        </Footer>
-      </div>
+      <Outlet />
       <ToastContainer
         position="top-left"
         autoClose={3000}
